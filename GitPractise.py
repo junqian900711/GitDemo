a8c8da67ea40e1f280d25b7c392c3ce3b2007ba0
junqian900711@gmail.com <1019042603QianJu>
2018-11-28 22:39:28 -0600

    2018-11-28 commit for open a new tab

--- GitPractise.py
+++ GitPractise.py
@@ -1,7 +1,23 @@
 #2018-11-26
-print("Hello world")
-"hjkhkjdsadsadsad"
-"dasd"
-print("Test etst")
-print("666")
-#print()
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import requests
+import json
+
+browser = webdriver.Chrome()
+browser.maximize_window()
+#driver = webdriver.Firefox()
+#browser.get("http://login.taobao.com")
+#windows 用Keys.CONTROL 如同ctrl+t
+#browser.find_element_by_tag_name('body').send_keys(Keys.CONTROL + 'T')
+browser.get("http://www.amazon.com")
+
+js='window.open("http://www.google.com");'
+browser.execute_script(js)
+print(js)
+#driver.get('http://mm.taobao.com/')
+url = "http://www.baidu.com"
+js_1 = 'window.open("'+ url + '");'
+browser.execute_script(js_1)
+print("js_1 is :",js_1)
